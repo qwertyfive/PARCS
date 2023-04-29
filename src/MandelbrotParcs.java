474516619b1105d872bf8a58fe76cab88e5fa738
qwertyfive <39852023+qwertyfive@users.noreply.github.com>
2023-04-29 16:38:09 +0300

    asda

--- src/MandelbrotParcs.java
+++ src/MandelbrotParcs.java
@@ -9,9 +9,9 @@ import javax.imageio.ImageIO;
 
 public class MandelbrotParcs implements AM {
 
-    public static final int WIDTH = 8000;
-    public static final int HEIGHT = 8000;
-    public static final int MAX_ITER = 10000;
+    public static final int WIDTH = 1920;
+    public static final int HEIGHT = 1080;
+    public static final int MAX_ITER = 2000;
 
     public static void main(String[] args) {
         task curtask = new task();
