53a4df13b68210bd95c29e067091c6701ad21b47
qwertyfive <39852023+qwertyfive@users.noreply.github.com>
2023-04-29 16:24:14 +0300

    csmkms

--- src/MandelbrotParcs.java
+++ src/MandelbrotParcs.java
@@ -1,42 +1,70 @@
-import parcs.AM;
-import parcs.AMInfo;
-import parcs.channel;
-import parcs.point;
-import parcs.task;
+import parcs.*;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.imageio.ImageIO;
 
 public class MandelbrotParcs implements AM {
 
-    public static final int HEIGHT = 800;
     public static final int WIDTH = 800;
-    public static final double ZOOM = 100.0;
+    public static final int HEIGHT = 800;
     public static final int MAX_ITER = 1000;
 
     public static void main(String[] args) {
         task curtask = new task();
-        curtask.addJarFile("MandelbrotWorker.jar");
+        curtask.addJarFile("MandelbrotParcs.jar");
         (new MandelbrotParcs()).run(new AMInfo(curtask, (channel)null));
+        curtask.end();
     }
 
     public void run(AMInfo info) {
-        int num_points = 4;
-        point[] points = new point[num_points];
-        channel[] channels = new channel[num_points];
-
-        for (int i = 0; i < num_points; i++) {
-            points[i] = info.createPoint();
-            channels[i] = points[i].createChannel();
-            points[i].execute("MandelbrotWorker");
-            channels[i].write(WIDTH);
-            channels[i].write(HEIGHT);
-            channels[i].write(ZOOM);
-            channels[i].write(MAX_ITER);
+        List<point> points = new ArrayList<>();
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                points.add(new point(j, i));
+            }
         }
 
-        int[][][] results = new int[num_points][][];
-        for (int i = 0; i < num_points; i++) {
-            results[i] = (int[][])channels[i].readObject();
+        BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+
+        for (point p : points) {
+            int x = p.x;
+            int y = p.y;
+
+            double zx = 0;
+            double zy = 0;
+            double cX = (x - 400) / 200.0;
+            double cY = (y - 400) / 200.0;
+            int iter = MAX_ITER;
+            double tmp;
+
+            while (zx * zx + zy * zy < 4 && iter > 0) {
+                tmp = zx * zx - zy * zy + cX;
+                zy = 2.0 * zx * zy + cY;
+                zx = tmp;
+                iter--;
+            }
+
+            image.setRGB(x, y, iter | (iter << 8));
         }
 
-        // Об'єднання результатів та подальша робота з ними (наприклад, відображення результатів)
+        try {
+            File output = new File("MandelbrotParcs.png");
+            ImageIO.write(image, "png", output);
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private static class point {
+        int x, y;
+
+        public point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
