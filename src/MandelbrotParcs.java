7a1875530523b0de3361bccef66d6b2f93af52c1
qwertyfive <39852023+qwertyfive@users.noreply.github.com>
2023-04-29 18:36:01 +0300

    wwww

--- src/MandelbrotParcs.java
+++ src/MandelbrotParcs.java
@@ -1,53 +1,61 @@
 import parcs.*;
-
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import javax.imageio.ImageIO;
 import java.util.ArrayList;
 import java.util.List;
-import javax.imageio.ImageIO;
 
 public class MandelbrotParcs implements AM {
 
     public static final int WIDTH = 3000;
     public static final int HEIGHT = 3000;
     public static final int MAX_ITER = 5000;
-    public static final int NUM_WORKERS = 4;
+    public static final int NUM_WORKERS = 4; // Кількість воркерів
 
     public static void main(String[] args) {
         task curtask = new task();
         curtask.addJarFile("MandelbrotParcs.jar");
-        (new MandelbrotParcs()).run(new AMInfo(curtask, (channel)null));
+        (new MandelbrotParcs()).run(new AMInfo(curtask, null));
         curtask.end();
     }
 
     public void run(AMInfo info) {
+        int rowsPerWorker = HEIGHT / NUM_WORKERS;
+        List<channel> channels = new ArrayList<>();
+
+        for (int i = 0; i < NUM_WORKERS; i++) {
+            int startRow = i * rowsPerWorker;
+            int endRow = (i + 1) * rowsPerWorker;
+            if (i == NUM_WORKERS - 1) {
+                endRow = HEIGHT;
+            }
+
+            channel c = info.createChannel(); // Використовуйте createChannel() з info
+            channels.add(c);
+
+            info.parent.write(new point(startRow, endRow)); // Використовуйте info.parent.write() з point
+
+            // Використовуйте c.write() замість p.write()
+            c.write(startRow);
+            c.write(endRow);
+        }
         BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 
-        List<AMInfo> workersInfo = new ArrayList<>();
         for (int i = 0; i < NUM_WORKERS; i++) {
-            point p = info.createPoint();
-            channel c = p.createChannel();
-            p.execute(new AM() {
-                @Override
-                public void run(AMInfo info) {
-                    int workerId = info.channel.readInt();
-                    int numWorkers = info.channel.readInt();
+            point p = (point) info.parent.readObject();
+            channel c = (channel) info.parent.readObject();
 
-                    int[][] colors = calculateMandelbrot(workerId, numWorkers);
-                    info.channel.writeObject(colors);
-                }
-            });
-            c.write(i);
-            c.write(NUM_WORKERS);
-            workersInfo.add(new AMInfo(p, c));
-        }
+            int startRow = p.startRow;
+            int endRow = p.endRow;
 
-        for (AMInfo workerInfo : workersInfo) {
-            int[][] colors = (int[][]) workerInfo.channel.readObject();
-            for (int y = 0; y < HEIGHT; y++) {
+            int[] results = (int[]) c.readObject();
+
+            for (int y = startRow; y < endRow; y++) {
                 for (int x = 0; x < WIDTH; x++) {
-                    image.setRGB(x, y, colors[y][x] | (colors[y][x] << 8));
+                    int index = (y - startRow) * WIDTH + x;
+                    int iter = results[index];
+                    image.setRGB(x, y, iter | (iter << 8));
                 }
             }
         }
@@ -60,29 +68,12 @@ public class MandelbrotParcs implements AM {
         }
     }
 
-    private int[][] calculateMandelbrot(int workerId, int numWorkers) {
-        int[][] colors = new int[HEIGHT][WIDTH];
+    private static class point implements java.io.Serializable {
+        int startRow, endRow;
 
-        for (int y = workerId; y < HEIGHT; y += numWorkers) {
-            for (int x = 0; x < WIDTH; x++) {
-                double zx = 0;
-                double zy = 0;
-                double cX = (x - 400) / 200.0;
-                double cY = (y - 400) / 200.0;
-                int iter = MAX_ITER;
-                double tmp;
-
-                while (zx * zx + zy * zy < 4 && iter > 0) {
-                    tmp = zx * zx - zy * zy + cX;
-                    zy = 2.0 * zx * zy + cY;
-                    zx = tmp;
-                    iter--;
-                }
-
-                colors[y][x] = iter;
-            }
+        public point(int startRow, int endRow) {
+            this.startRow = startRow;
+            this.endRow = endRow;
         }
-
-        return colors;
     }
 }
