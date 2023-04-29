a20006911564790fe370c34d2609b7e9f39752e6
qwertyfive <39852023+qwertyfive@users.noreply.github.com>
2023-04-29 16:56:23 +0300

    asdasdas

--- Makefile
+++ Makefile
@@ -1,16 +1,21 @@
 all: run
 
 clean:
-	rm -f out/MandelbrotParcs.jar
+	rm -f out/MandelbrotParcs.jar out/MandelbrotWorker.jar
 
 out/MandelbrotParcs.jar: src/MandelbrotParcs.java
 	@javac -cp out/parcs.jar src/MandelbrotParcs.java
 	@jar cf out/MandelbrotParcs.jar -C src .
 	@rm -f src/MandelbrotParcs.class
 
-build: out/MandelbrotParcs.jar
+out/MandelbrotWorker.jar: src/MandelbrotWorker.java
+	@javac -cp out/parcs.jar src/MandelbrotWorker.java
+	@jar cf out/MandelbrotWorker.jar -C src .
+	@rm -f src/MandelbrotWorker.class
 
-run: out/MandelbrotParcs.jar
+build: out/MandelbrotParcs.jar out/MandelbrotWorker.jar
+
+run: out/MandelbrotParcs.jar out/MandelbrotWorker.jar
 	@cd out && java -cp 'parcs.jar:MandelbrotParcs.jar' MandelbrotParcs
 
 .PHONY: all build run clean
--- src/MandelbrotParcs.java
+++ src/MandelbrotParcs.java
@@ -9,46 +9,50 @@ import javax.imageio.ImageIO;
 
 public class MandelbrotParcs implements AM {
 
-    public static final int WIDTH = 3000;
-    public static final int HEIGHT = 3000;
-    public static final int MAX_ITER = 5000;
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 800;
+    public static final int MAX_ITER = 1000;
+    public static final int NUM_WORKERS = 4; // Кількість воркерів
 
     public static void main(String[] args) {
         task curtask = new task();
         curtask.addJarFile("MandelbrotParcs.jar");
+        curtask.addJarFile("MandelbrotWorker.jar");
         (new MandelbrotParcs()).run(new AMInfo(curtask, (channel)null));
         curtask.end();
     }
 
     public void run(AMInfo info) {
-        List<point> points = new ArrayList<>();
-        for (int i = 0; i < HEIGHT; i++) {
-            for (int j = 0; j < WIDTH; j++) {
-                points.add(new point(j, i));
-            }
-        }
-
         BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 
-        for (point p : points) {
-            int x = p.x;
-            int y = p.y;
+        int rowsPerWorker = HEIGHT / NUM_WORKERS;
+        List<channel> channels = new ArrayList<>();
 
-            double zx = 0;
-            double zy = 0;
-            double cX = (x - 400) / 200.0;
-            double cY = (y - 400) / 200.0;
-            int iter = MAX_ITER;
-            double tmp;
+        for (int i = 0; i < NUM_WORKERS; i++) {
+            int startRow = i * rowsPerWorker;
+            int endRow = i == NUM_WORKERS - 1 ? HEIGHT : (i + 1) * rowsPerWorker;
 
-            while (zx * zx + zy * zy < 4 && iter > 0) {
-                tmp = zx * zx - zy * zy + cX;
-                zy = 2.0 * zx * zy + cY;
-                zx = tmp;
-                iter--;
+            point p = info.createPoint();
+            channel c = p.createChannel();
+            channels.add(c);
+
+            p.execute("MandelbrotWorker");
+
+            c.write(startRow);
+            c.write(endRow);
+        }
+
+        for (int i = 0; i < NUM_WORKERS; i++) {
+            int startRow = i * rowsPerWorker;
+            int[] results = (int[]) channels.get(i).readObject();
+
+            for (int y = startRow; y < startRow + rowsPerWorker && y < HEIGHT; y++) {
+                for (int x = 0; x < WIDTH; x++) {
+                    int index = (y - startRow) * WIDTH + x;
+                    int iter = results[index];
+                    image.setRGB(x, y, iter | (iter << 8));
+                }
             }
-
-            image.setRGB(x, y, iter | (iter << 8));
         }
 
         try {
@@ -59,12 +63,21 @@ public class MandelbrotParcs implements AM {
         }
     }
 
-    private static class point {
-        int x, y;
+    public static int calculatePoint(int x, int y) {
+        double zx = 0;
+        double zy = 0;
+        double cX = (x - 400) / 200.0;
+        double cY = (y - 400) / 200.0;
+        int iter = MAX_ITER;
+        double tmp;
 
-        public point(int x, int y) {
-            this.x = x;
-            this.y = y;
+        while (zx * zx + zy * zy < 4 && iter > 0) {
+            tmp = zx * zx - zy * zy + cX;
+            zy = 2.0 * zx * zy + cY;
+            zx = tmp;
+            iter--;
         }
+
+        return iter;
     }
 }
