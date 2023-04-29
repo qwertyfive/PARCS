5c55863d319f53f52b2a91cc3b5a55e0411039f0
qwertyfive <39852023+qwertyfive@users.noreply.github.com>
2023-04-29 18:32:04 +0300

    haaalo

--- src/MandelbrotParcs.java
+++ src/MandelbrotParcs.java
@@ -12,43 +12,36 @@ public class MandelbrotParcs implements AM {
     public static final int WIDTH = 3000;
     public static final int HEIGHT = 3000;
     public static final int MAX_ITER = 5000;
+    public static final int NUM_WORKERS = 4;
 
     public static void main(String[] args) {
         task curtask = new task();
         curtask.addJarFile("MandelbrotParcs.jar");
-        (new MandelbrotParcs()).run(new AMInfo(curtask, (channel)null));
+        (new MandelbrotParcs()).run(new AMInfo(curtask, (channel) null));
         curtask.end();
     }
 
     public void run(AMInfo info) {
-        List<point> points = new ArrayList<>();
-        for (int i = 0; i < HEIGHT; i++) {
-            for (int j = 0; j < WIDTH; j++) {
-                points.add(new point(j, i));
-            }
-        }
-
         BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 
-        for (point p : points) {
-            int x = p.x;
-            int y = p.y;
+        List<AMInfo> workersInfo = new ArrayList<>();
+        for (int i = 0; i < NUM_WORKERS; i++) {
+            point p = info.createPoint();
+            channel c = p.createChannel();
+            p.addExecuteClass(this.getClass());
+            p.start("MandelbrotParcs", "runWorker");
+            c.write(i);
+            c.write(NUM_WORKERS);
+            workersInfo.add(new AMInfo(p, c));
+        }
 
-            double zx = 0;
-            double zy = 0;
-            double cX = (x - 400) / 200.0;
-            double cY = (y - 400) / 200.0;
-            int iter = MAX_ITER;
-            double tmp;
-
-            while (zx * zx + zy * zy < 4 && iter > 0) {
-                tmp = zx * zx - zy * zy + cX;
-                zy = 2.0 * zx * zy + cY;
-                zx = tmp;
-                iter--;
+        for (AMInfo workerInfo : workersInfo) {
+            int[][] colors = (int[][]) workerInfo.parent.readObject();
+            for (int y = 0; y < HEIGHT; y++) {
+                for (int x = 0; x < WIDTH; x++) {
+                    image.setRGB(x, y, colors[y][x] | (colors[y][x] << 8));
+                }
             }
-
-            image.setRGB(x, y, iter | (iter << 8));
         }
 
         try {
@@ -59,12 +52,36 @@ public class MandelbrotParcs implements AM {
         }
     }
 
-    private static class point {
-        int x, y;
+    public void runWorker(AMInfo info) {
+        int workerId = info.parent.readInt();
+        int numWorkers = info.parent.readInt();
 
-        public point(int x, int y) {
-            this.x = x;
-            this.y = y;
+        int[][] colors = calculateMandelbrot(workerId, numWorkers);
+        info.parent.writeObject(colors);
+    }
+
+    private int[][] calculateMandelbrot(int workerId, int numWorkers) {
+        int[][] colors = new int[HEIGHT][WIDTH];
+
+        for (int y = workerId; y < HEIGHT; y += numWorkers) {
+            for (int x = 0; x < WIDTH; x++) {
+                double zx = 0;
+                double zy = 0;
+                double cX = (x - 400) / 200.0;
+                double cY = (y - 400) / 200.0;
+                int iter = MAX_ITER;
+                double tmp;
+
+                while (zx * zx + zy * zy < 4 && iter > 0) {
+                    tmp = zx * zx - zy * zy + cX;
+                    zy = 2.0 * zx * zy + cY;
+                    zx = tmp;
+                    iter--;
+                    colors[y][x] = iter;
+                }
+            }
+
+            return colors;
         }
     }
 }
