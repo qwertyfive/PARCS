9663cf649f02876a625d9f70008f4bcdac7dd400
qwertyfive <39852023+qwertyfive@users.noreply.github.com>
2023-04-30 14:22:22 +0300

    changes

--- src/Solver.java
+++ src/Solver.java
@@ -26,8 +26,8 @@ public class Solver implements AM {
     }
 
     public void run(AMInfo info) {
-        long range = 1000000000;
-        int workers = 6;
+        long range = 10000000;
+        int workers = 2;
         long tStart = System.nanoTime();
 
         long res = solve(info, range, workers);
